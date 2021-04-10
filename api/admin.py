5390ffa83dd7e8fef00be80707ab0d35c66cb552
Satripleen <satripleen@gmail.com>
2021-04-09 22:59:43 -0400

    add device model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from api.models import Device
+
+admin.site.register(Device)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Device(models.Model):
+    name = models.CharField(max_length=20)
+    device_tag = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
